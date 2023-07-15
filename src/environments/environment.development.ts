b5b80eb075b75f0620a946c8c805d219ac3118f2
Anastasia Karachova <57650422+undefined-person@users.noreply.github.com>
2023-07-15 05:42:58 +0300

    fix: predeployment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  baseUrl: 'http://api.openweathermap.org',
+  baseUrl: 'https://api.openweathermap.org',
   apiKey: 'e9fd52f7df0fe07f15658c3281fd9724'
 };
